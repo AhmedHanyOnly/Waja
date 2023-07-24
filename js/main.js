8702c308823b73df8a01bf522a005b2078a794c0
ahmed <mommentcraft@gmail.com>
2023-07-24 20:42:10 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-// Btn Add Active
-let addActive = document.querySelectorAll("#add-active");
-addActive.forEach(btn=> {
- btn.addEventListener("click",()=> {
-  let element = document.querySelector(btn.dataset.active);
-  element.classList.toggle("active")
- })
-})
+// Initialize Swiper Slider
+// Set Landing Page Slider
+const swiperElement = document.querySelector(".landing-swiper")
+new Swiper(swiperElement, {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  parallax: true,
+  speed: 1400,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  
+});
 
-// close popup Video
-let closeVideo = document.querySelector(".modal-video-wrapper .video-close");
-let modalContent = document.querySelector(
-    ".modal-video-wrapper .modal-content"
-);
-let oldIframe = document.querySelector(".modal-video-wrapper iframe");
-let newIframe = oldIframe;
-closeVideo.addEventListener("click", function () {
-    oldIframe.remove();
-    modalContent.appendChild(newIframe);
+new Swiper(".customers-slider", {
+  slidesPerView: 1,
+  spaceBetween: 21,
+  loop: true,
+  speed: 1300,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
 });
